9766b2daa1168f001b7ce7f3e026d5a938745d4b
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-06-07 13:09:07 -0400

    commit tree ae6088aa73eb8666e419b5052e1cf8b2e2238085

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2 
+let random = ""
+random = parseInt(Math.random(), 10) + 1
+const num3 = 29
+const num4 = 5
+const mod = num3 % num4
+const max = Math.max(1, 10, 20)
